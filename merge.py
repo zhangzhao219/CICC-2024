0f32e778a380203e6c1fb219b5eee683318a35c0
zhangzhao219 <zhaozhao809@163.com>
2024-10-29 17:24:47 +0800

    accelerate

--- merge.py
+++ merge.py
@@ -47,32 +47,25 @@ MERGE_FILE_LIST = [
     "2024_10_27_22_10_51",  # 0.3460207473671952
     "2024_10_27_22_12_21",  # 0.34171560905285675
     # qwen25_ernie
+    "2024_10_28_08_20_46",  # 0.36719007229339046
     "2024_10_27_12_13_17",  # 0.35323439085204933
     "2024_10_26_10_37_05",  # 0.3651463253824448
     "2024_10_25_15_33_35",  # 0.367002014520443
     "2024_10_26_23_25_05",  # 0.38045089759885403
+    "2024_10_28_21_08_36",  # 0.37277270123503375
     # qwen2textcnn
     "2024_10_26_01_14_18",  # 0.34349508388742184
     "2024_10_27_20_03_09",  # 0.34054465199967954
     "2024_10_27_05_45_03",  # 0.3489551952271762
+    "2024_10_28_21_17_39",  # 0.3477689605241899
     # qwen2_ernie
     "2024_10_27_12_13_29",  # 0.3529041491583787
     "2024_10_26_10_37_22",  # 0.34693583319320426
     "2024_10_26_23_25_11",  # 0.35723913070429025
     # yi_ernie
     "2024_10_26_04_21_48",  # 0.36280445373166537
+    "2024_10_28_07_30_06",  # 0.34965055869741535
 ]
-
-
-def combine(temp_list):
-    end_list = []
-    for i in range(2, len(MERGE_FILE_LIST)):
-        print(i)
-        temp_list2 = []
-        for c in combinations(temp_list, i):
-            temp_list2.append(list(c))
-        end_list.extend(temp_list2)
-    return end_list
 
 
 emotion_dict = {
@@ -97,53 +90,65 @@ if MODE == "val":
 
 result_data_list = []
 
+for index, file in enumerate(MERGE_FILE_LIST):
+    result_data_list.append(
+        pd.read_csv(os.path.join(RESULT_FOLDER, f"{MODE}_data_{file}.csv"))[
+            ["0", "1", "2", "3", "4", "5", "6"]
+        ].to_numpy()
+    )
+
+result_data_list = np.array(result_data_list)
+
+
+def combine(temp_list):
+    end_list = []
+    for i in tqdm(range(2, 6)):
+        a = combinations([i for i in range(len(temp_list))], i)
+        for c in tqdm(list(a)):
+            now_list = [False for i in range(len(temp_list))]
+            for c2 in c:
+                now_list[c2] = True
+            end_list.append(np.array(now_list))
+    return end_list
+
+
+SUB_MERGE_FILE_LIST = combine(MERGE_FILE_LIST)
+
 max_score = 0
 NOW_MERGE_FILE_LIST = []
 
-for SUB_MERGE_FILE_LIST in tqdm(combine(MERGE_FILE_LIST)):
-    for result_file in SUB_MERGE_FILE_LIST:
-        # print(result_file)
-        result_data = pd.read_csv(
-            os.path.join(RESULT_FOLDER, f"{MODE}_data_{result_file}.csv")
-        )
-        result_data_list.append(result_data.to_numpy())
-        if MODE == "val":
-            result_data["label"] = result_data["7"]
-            result_data["predict"] = result_data[
-                ["0", "1", "2", "3", "4", "5", "6"]
-            ].idxmax(axis=1)
-            # print(
-            #     classification_report(
-            #         result_data["label"],
-            #         result_data["predict"].astype(int),
-            #         output_dict=True,
-            #     )["macro avg"]["f1-score"]
-            # )
+for zero_one_select in tqdm(SUB_MERGE_FILE_LIST):
+    now_file_list = result_data_list[zero_one_select]
 
-        # print(
-        #     classification_report(
-        #         result_data["7"].map(emotion_dict), ori_data_label, output_dict=True, zero_division=0
-        #     )
-        # )
-    # print(np.array(result_data_list).mean(axis=0))
-    final_result = pd.DataFrame(np.array(result_data_list).mean(axis=0))
-    # final_result.columns = ["0", "1", "2", "3", "4", "5", "6", "7"]
-    final_result["predict"] = (
-        final_result[[0, 1, 2, 3, 4, 5, 6]].idxmax(axis=1).astype(int).map(emotion_dict)
-    )
+    # if MODE == "val":
+    #     for i in range(now_file_list.shape[0]):
+    #         ids = np.argmax(now_file_list[i], axis=1)
+    #         ids_str = np.array(list(map(emotion_dict.get, ids)))
+    #         print(
+    #             classification_report(
+    #                 ori_data_label, ids_str, output_dict=True, zero_division=0
+    #             )["macro avg"]["f1-score"]
+    #         )
+
+    now_file_list = now_file_list.sum(axis=0)
+    ids = np.argmax(now_file_list, axis=1)
+    ids_str = np.array(list(map(emotion_dict.get, ids)))
 
     if MODE == "val":
         score = classification_report(
-            ori_data_label, final_result["predict"], output_dict=True, zero_division=0
+            ori_data_label, ids_str, output_dict=True, zero_division=0
         )["macro avg"]["f1-score"]
         if score > max_score:
             max_score = score
-            NOW_MERGE_FILE_LIST = SUB_MERGE_FILE_LIST
+            NOW_MERGE_FILE_LIST = []
+            for i, j in enumerate(zero_one_select):
+                if j == 1:
+                    NOW_MERGE_FILE_LIST.append(MERGE_FILE_LIST[i])
             print(score, NOW_MERGE_FILE_LIST)
 
 
 if MODE == "test":
-    predict_labels = final_result["predict"].tolist()
+    predict_labels = ids_str.tolist()
     global_index = 0
     for k1, v1 in ori_data.items():
         for k2, v2 in v1.items():
